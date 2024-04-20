2f3fc10cb2dad4382893013f14fbf2c3a3d97158
Sharerp <sharerp@DESKTOP-7B95SI3>
2024-04-21 00:12:33 +0200

    no hay debugger todavia

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,18 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    term_colors = true,
+    dim_inactive = {
+      enabled = false, -- dims the background color of inactive window
+      shade = "dark",
+      percentage = 0.15, -- percentage of the shade to apply to the inactive window
+    },
+  },
+
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin")
+  end,
+}
---
+++ lua/plugins/telescope-live-grep-args.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      -- This will not install any breaking changes.
+      -- For major updates, this must be adjusted manually.
+      version = "^1.0.0",
+    },
+  },
+  config = function()
+  vim.keymap.set("n", "<C-f>", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+  require("telescope").load_extension("live_grep_args")
+  end,
+}
